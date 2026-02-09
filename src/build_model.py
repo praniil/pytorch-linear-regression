0a22077ebe936763e691db5c16856fb1d754351a
praniil <pparajuli15@gmail.com>
2026-02-09 22:28:20 +0545

    update: wrapped the main init logic for model in a function

--- src/build_model.py
+++ src/build_model.py
@@ -1,29 +1,32 @@
 import torch.nn as nn
 import torch    
-
 class LinearRegressionModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.weights = nn.Parameter(torch.randn(1, dtype=torch.float), requires_grad=True)
         self.bias = nn.Parameter(torch.randn(1, dtype=torch.float), requires_grad=True)
-
     def forward(self, x:torch.Tensor) -> torch.Tensor:
         return self.weights * x + self.bias
 
-#setting the manual seed
-torch.manual_seed(42)
-model = LinearRegressionModel()
-print(model.weights)
-print(model)
-print(model.state_dict())
+def init_model():
+    #setting the manual seed
+    torch.manual_seed(42)
+    model = LinearRegressionModel()
+    print(model.weights)
+    print(model)
+    print(model.state_dict())
 
 
-# check the model device
-print(next(model.parameters()).device)
+    # check the model device
+    print(next(model.parameters()).device)
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# changing it to the gpu
-model.to(device=device)
-print(next(model.parameters()).device)
+    # changing it to the gpu
+    model.to(device=device)
+    print(next(model.parameters()).device)
+    
+    return model
 
+if __name__ == "__main__":
+    init_model()
